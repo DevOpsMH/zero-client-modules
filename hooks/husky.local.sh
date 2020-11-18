a18f252f4a94e0b3103d535bd7d3feb21b705a14
boomerangll <yaronmironboomerangll@gmail.com>
2020-11-18 16:09:23 +0200

    added husky

---
+++ hooks/husky.local.sh
@@ -0,0 +1,6 @@
+# Created by Husky v4.3.0 (https://github.com/typicode/husky#readme)
+#   At: 11/18/2020, 1:59:07 PM
+#   From: undefined (https://github.com/typicode/husky#readme)
+
+packageManager=npm
+cd "."
